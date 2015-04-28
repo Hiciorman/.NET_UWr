91dddad8b906d81e4aa749eb01eb22f56d106d4f
Szymon <hiciorman@gmail.com>
2015-04-28 23:07:11 +0200

    Undone

---
+++ 2.4/2.4.8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            //TODO
+            foreach (var item in list.Select(i => 1))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
