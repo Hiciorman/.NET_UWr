d8483bfd197592831b8575bad8a701e7b49d0ddf
Szymon <hiciorman@gmail.com>
2015-05-26 23:17:26 +0200

    Controls

---
+++ 3.3/3.3.1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3._3._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (var item in ComboBoxSource)
+            {
+                comboCykl.Items.Add(item);                
+            }
+        }
+
+        private string[] comboBoxSource = { "3-letnie", "4-letnie", "5-letnie", "6-letnie" };
+
+        public string[] ComboBoxSource
+        {
+            get { return comboBoxSource; }
+            set { comboBoxSource = value; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(String.Format("{0}\n{1}\n{2}\n{3} {4}", txtNazwa.Text, txtAdres.Text, comboCykl.SelectedValue, (bool)checkBoxD.IsChecked ? checkBoxD.Content : "", (bool)checkBoxU.IsChecked ? checkBoxU.Content : ""));
+        }
+
+        private void buttonAnuluj_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ 3.3/3.3.3/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace _3._3._3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MessageDialog message;
+        private string[] listItems = new string[] { "a", "b", "c", "d" };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            this.listView.ItemsSource = this.listItems;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            message = new MessageDialog("Pin button");
+            await message.ShowAsync();
+        }
+
+        private async void AppBarButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            message = new MessageDialog("Emoji button");
+            await message.ShowAsync();
+        }
+
+        private async void AppBarButton_Click_2(object sender, RoutedEventArgs e)
+        {
+            message = new MessageDialog(".NET button");
+            await message.ShowAsync();
+        }
+    }
+}
